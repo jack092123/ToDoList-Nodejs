e7510993ea92dc643875432e0e29ef425fb9178b
RoachOwO <jack092123@gmail.com>
2018-03-31 11:19:30 +0800

    Fix the Logging

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use('/public', express.static(__dirname + '/public'));
 
 //Log all request to the console
 app.all('*', function(req, res, next){
-    console.log("Method: " + req.method + "; IP: " + req.ip);
+    console.log("Method: " + req.method + "; IP: " + req.connection.remoteAddress);
     next();
 });
 
